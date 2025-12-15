fc47f13492c7e9440c98cf7d1b9fecda0e4820be
eikoeigoenglish-star <228509965+eikoeigoenglish-star@users.noreply.github.com>
2025-12-15 22:09:03 +0900

    Update GAS_API_EXEC_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.__mark && window.__mark('config.js');
 
 window.APP_CONFIG = {
-  GAS_API_EXEC_URL: "https://script.google.com/macros/s/AKfycbwgNLtoYb0DRFuivrUGSh-L9g2XfNjkr0YLlOxfO5bgQH7afC0_TjdJcK0zHlzSAft9/exec",
+  GAS_API_EXEC_URL: "https://script.google.com/macros/s/AKfycbyASFVkWkmyeK66QIMo4GX6LWsLLQ4RECHzrXiJLjbNcRFaHniK7Zwny9ZeHlrMblwM/exec",
   BASE_DATE: "2025-11-01",
   USERS: ["Cさん","Sさん","Hさん","Yさん","Aさん","Dさん"]
 };
