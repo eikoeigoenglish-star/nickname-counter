83c1019c7c4e632b8a88b1204c62eeb135bebe70
eikoeigoenglish-star <228509965+eikoeigoenglish-star@users.noreply.github.com>
2025-12-15 20:40:37 +0900

    Update GAS API execution URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ここを自分のGAS WebアプリURLに差し替え（/exec）
 window.APP_CONFIG = {
   GAS_API_EXEC_URL: "window.APP_CONFIG = {
-  GAS_API_EXEC_URL: "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLg1xae7FdH_IS2v2fVO7jWbiAPgPg2ThtKUsYRnYgAWSumYUo70DrUN3cynPXdAz1hXRwuktXn6O1JDADizZL0wrUWKh1lAlXo3ej-5ZjqCY9QmPG9lVwQMlRFHWYxvuD7jDUsGkUawytrr3DFn95ebDSWL3fQMMqJHb4tcipBFntp4Btowi0JaeByrBwj6GMzGvh13xmXcBOHApZf1gkW7NHQv76Xp-2yKhHKM2UzPGUA2q_b2pGTs08XOSj9gDnNmAz8NJ_ZuFOHqfGQL_dR03Z_ejA", // ←これ
+  GAS_API_EXEC_URL: "https://script.google.com/macros/s/AKfycbwgNLtoYb0DRFuivrUGSh-L9g2XfNjkr0YLlOxfO5bgQH7afC0_TjdJcK0zHlzSAft9/exec", // ←これ
   BASE_DATE: "2025-11-01",
   USERS: ["Cさん","Sさん","Hさん","Yさん","Aさん","Dさん"]
 };
