ef9b670eefbff23a45ccdc71c3fbe4394a7a9611
eikoeigoenglish-star <228509965+eikoeigoenglish-star@users.noreply.github.com>
2025-12-15 20:22:35 +0900

    Update GAS_API_EXEC_URL and configuration values
    Updated GAS_API_EXEC_URL and modified BASE_DATE and USERS.

--- js/config.js
+++ js/config.js
@@ -2,10 +2,6 @@
 window.APP_CONFIG = {
   GAS_API_EXEC_URL: "window.APP_CONFIG = {
   GAS_API_EXEC_URL: "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLgOaTv8NrVOovjTg_J_7bb8o-rbfbPpJ0TKMkq6iBXDCpiWbNpsKugkAl8PVBvl882Ksx6ew0q0yFNVwAXtzqIxWozYnxGYpful1bVleDXO93T0_gVzVbavt0jJH81YG1Mj5aPo2imD_DRNHbteVyq9Kc2e_d4g-6KWyMhrlvnT2y9A19el_S1i4z7iA4QySpf6_nmwrUNIhzjQVGXkjEEb0rcVuv_JXWt0_ck_vdWw98lbGEZL7AemDW6jgbxFWUcyYaBTjck0sGSgdUfXIH14FNVR3g&lib=MeJfdE9NJGKVRfsWrqnxYSiMNGS6H3WL4", // ←これ
-  BASE_DATE: "2026-01-01",
-  USERS: ["Aさん","Bさん","Cさん","Dさん","Eさん","Fさん"]
-};
-", // ←スクショのやつ
   BASE_DATE: "2025-11-01",
   USERS: ["Cさん","Sさん","Hさん","Yさん","Aさん","Dさん"]
 };
