0b0b2f1483ff083687e675b2876e62065d845f65
eikoeigoenglish-star <228509965+eikoeigoenglish-star@users.noreply.github.com>
2025-12-15 20:04:03 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // ここを自分のGAS WebアプリURLに差し替え（/exec）
 window.APP_CONFIG = {
-  GAS_API_EXEC_URL: "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLhTDnbCOyamk6X0jMY6R5UKOxVJJaRFzFUisiHcpnhcXy66w--fzW95ALJ5AG7_-tLYKJsH0SBkZe_y-JO0SAHx-vmGBt5Dby8JEGXx_ClQsQ5iJnFaxxFeLKkhHXwhWd2IRfagfClO4VoYUVOd2wBczfmfyU3Ay8KXi-Hg2S0k1UoBTKbSEGMT4nFsNTKyPX3i7st0OymuiE6y3i9r2q8nDhAtg7dYSWAFM6BGn5CI43XMXR2HEarQtWHkdbJRqgtqQ73kjZmpbXtAwF7j7sJ272ZJag&lib=MeJfdE9NJGKVRfsWrqnxYSiMNGS6H3WL4",
+  GAS_API_EXEC_URL: "https://script.googleusercontent.com/macros/echo?user_content_key=XXXX...", // ←スクショのやつ
   BASE_DATE: "2026-01-01",
   USERS: ["Aさん","Bさん","Cさん","Dさん","Eさん","Fさん"]
 };
