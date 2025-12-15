204c050b6d54bad8d55c11ad352ea601306383d3
eikoeigoenglish-star <228509965+eikoeigoenglish-star@users.noreply.github.com>
2025-12-15 20:10:39 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // ここを自分のGAS WebアプリURLに差し替え（/exec）
 window.APP_CONFIG = {
   GAS_API_EXEC_URL: "https://script.googleusercontent.com/macros/echo?user_content_key=XXXX...", // ←スクショのやつ
-  BASE_DATE: "2026-01-01",
+  BASE_DATE: "2025-11-01",
   USERS: ["Aさん","Bさん","Cさん","Dさん","Eさん","Fさん"]
 };
