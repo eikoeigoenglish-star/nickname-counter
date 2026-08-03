a4701a7081e69e724f7943cb954a7e3501c0a1ff
eikoeigoenglish-star <228509965+eikoeigoenglish-star@users.noreply.github.com>
2026-08-03 19:54:43 +0900

    Update GAS_API_EXEC_URL in APP_CONFIG

--- js/config.js
+++ js/config.js
@@ -1,5 +1,3 @@
-window.APP_CONFIG = Object.freeze({
-  GAS_API_EXEC_URL: 'https://script.google.com/macros/s/AKfycbz39CzpQ4Z_2QhoeLN4UREVxsREWyWSJsUnfe03r1QhSeCNMNTJ6jAbIo9eAdIL1xzG/exec',
-  JSONP_TIMEOUT_MS: 15000,
-  JSONP_RETRIES: 1,
-});
+window.APP_CONFIG = {
+  GAS_API_EXEC_URL: "https://script.google.com/macros/s/AKfycbxfUkxlAC0qq9tY3c6FCkKHN95F__msHGm_-4fuJbeL4vrwpfDABXlJGgRuQ_0fY29w/exec"
+};
